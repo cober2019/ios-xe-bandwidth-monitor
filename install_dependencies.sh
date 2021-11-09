5a813807f7e670ae3e54f706c421e8ff1a18b71d
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-11-09 00:05:49 -0500

    Add files via upload

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+#IF YOU DONT HAVE THE FOLLOWING DEPENDENCIES INSTALLED THEN RUN THIS SCRIPT, sudo bash install_dependencies
+sudo apt update
+INSTALL_PKGS="python3.10 nodejs npm openssl python3.8-venv pip"
+for i in $INSTALL_PKGS; do
+  sudo apt-get install -y  $i
+done
