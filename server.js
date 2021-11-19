f8dac3ff581bab3662485d41f2a90219b2854d04
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-11-19 12:46:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.post('/login', createProxyMiddleware({ target: 'https://127.0.0.1:5000', changeOrigin: true, secure: false }));
 app.post('/liveinterfaces', createProxyMiddleware({ target: 'https://127.0.0.1:5000', changeOrigin: true , secure: false }));
+app.post('/interface_stats_ietf', createProxyMiddleware({ target: 'https://127.0.0.1:5000', changeOrigin: true , secure: false }));
 
 app.get('/*', function (req, res) {res.sendFile(path.join(__dirname, 'build', 'index.html'));});
 
