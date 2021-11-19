89842e81f0f865a798bde5ce6f160498b3706ca7
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-11-19 00:23:16 -0500

    Add files via upload

--- BackEndModules/BandwidthTool.py
+++ BackEndModules/BandwidthTool.py
@@ -24,6 +24,8 @@ def _check_api_error(response) -> bool:
 def get_interface_details(ip, port, username, password) -> list:
     """Gets interface IP information using IETF model"""
 
+    interfaces_details = []
+    
     try:
         uri = f"https://{ip}:{port}/restconf/data/ietf-interfaces:interfaces/interface"
         response = requests.get(uri, headers=headers, verify=False, auth=(username, password))
@@ -34,30 +36,33 @@ def get_interface_details(ip, port, username, password) -> list:
         if check_error:
             raise AttributeError
 
-        device_interfaces = interfaces.get('ietf-interfaces:interface', {})
+        interfaces_details = interfaces.get('ietf-interfaces:interface', {})
 
     except (JSONDecodeError, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL, AttributeError) as e:
         pass
 
-    return device_interfaces
+    return interfaces_details
 
 def get_interface_stats(ip, port, username, password) -> list:
-    """Collects device vrfs"""
+    """Collects interface stats"""
 
+    interfaces_stats = []
 
     try:
         uri = f"https://{ip}:{port}/restconf/data/ietf-interfaces:interfaces-state"
         response = requests.get(uri, headers=headers, verify=False, auth=(username, password))
         interfaces = json.loads(response.text)
 
+        #Check for valid json, raise error if data isnt valid
         check_error = _check_api_error(interfaces)
-
         if check_error:
             raise AttributeError
 
         interfaces_stats = interfaces.get('ietf-interfaces:interfaces-state', {}).get('interface', {})
+        #Since the interface-state yang model doesnt give ip, make another api call to get the details
         interface_details = get_interface_details(ip, port, username, password)
 
+        #Iterate through bother lists and add k/v pairs for the missing data
         for ints_stats, int_details in zip(interfaces_stats, interface_details):
             try:
                 ints_stats['ip_address'] = f"{int_details.get('ietf-ip:ipv4', {}).get('address', {})[0].get('ip')} {int_details.get('ietf-ip:ipv4', {}).get('address', {})[0].get('netmask')}"
@@ -86,18 +91,21 @@ class CalcBandwidth:
         self.poll_interval = 0
 
 
-    def get_interface_bandwith_all(self):
+    def get_interface_bandwith_all(self) -> list:
         """Calculate outbound bandwidth"""
 
         mbps_out = 0
+        #Get Interface stats
         interface_stats = get_interface_stats(self.host, self.port, self.username, self.password)
 
+        #Check interval, the program will show really larger numbers if will dont skip interval one
         if self.last_poll is not None or self.poll_interval > 1:
             self.poll_interval = 2
             for interface in interface_stats:
                 for i in self.last_poll:
                     if interface.get("name") == i.get("name"):
                         try:
+                            #Calculate stats subtract old stats from new. Conver from bytes to bits and divide. O yea, add new k/v to dictionary witht the calculated value
                             bytes_out_diff =  int(interface.get("statistics").get("out-octets", {})) - int(i.get('previos_octets_out', 0))
                             interface.update({'previos_octets_out': interface.get("statistics").get("out-octets", {})})
                             calc_1 = bytes_out_diff * 8 / 20
@@ -111,7 +119,7 @@ class CalcBandwidth:
                         except (AttributeError, OSError, ValueError) as e:
                             pass
                         finally:
-
+                            # We dont want negative number. This will happen of there is a counter rollover
                             if mbps_out < 0:
                                 mbps_out = 0
 
@@ -121,15 +129,16 @@ class CalcBandwidth:
                                 mbps_in = 0
 
                             interface['mbps_in'] = mbps_in
-
+            # store the modifed data structure as our last poll for comparison with next poll
             self.last_poll = interface_stats
 
         else:
             self.poll_interval = self.poll_interval =+ 1
+            #Create new k/v for next poll calculations
             for interface in interface_stats:
                 interface['previos_octets_out'] = int(interface.get("statistics").get("out-octets", {}))
                 interface['previos_octets_in'] = int(interface.get("statistics").get("in-octets", {}))
-
+            #store the modifed data structure as our last poll for comparison with next poll
             self.last_poll = interface_stats
 
 
